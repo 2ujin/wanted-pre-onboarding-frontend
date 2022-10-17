a6aee0baa09371ae2d43dee7bdba60ea6bc301b1
2ujin <suddenlydisappear200@gmail.com>
2022-10-17 20:46:21 +0900

    회원가입 마무리

--- src/page/SignUp/SignUp.tsx
+++ src/page/SignUp/SignUp.tsx
@@ -1,3 +1,5 @@
+import axios from "axios";
+import { useState } from "react";
 import { useHistory } from "react-router-dom";
 import styled from "styled-components";
 
@@ -22,10 +24,9 @@ const IdInput = styled.input`
 
 const PassWordInput = styled.input``;
 
-const SubmitBtn = styled.div`
+const SubmitBtn = styled.button`
   width: 100%;
   border-radius: 20px;
-  background-color: blue;
   padding: 15px;
   text-align: center;
   color: white;
@@ -39,15 +40,83 @@ const GoSignUp = styled.div`
 `;
 
 function SignUp() {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+  const { email, password } = form;
+
+  let is_disable = true;
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    if (name === "email" && value.includes("@")) {
+      is_disable = false;
+    }
+
+    if (name === "password" && value.length > 7) {
+      is_disable = false;
+    }
+
+    console.log(is_disable);
+
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+
+  const clickSignUp = () => {
+    try {
+      const option = {
+        url: " https://pre-onboarding-selection-task.shop/auth/signup",
+        method: "POST",
+        header: {
+          "Content-Type": "application/json",
+        },
+        data: {
+          email: form.email,
+          password: form.password,
+        },
+      };
+      axios(option).then((response) => {
+        const access_token = response.data.access_token;
+        if (!access_token) {
+          alert("회원가입에 실패하셨습니다");
+          return;
+        }
+
+        console.log(access_token);
+      });
+    } catch (error) {
+      //응답 실패
+      console.error(error);
+    }
+  };
+
   return (
     <Wrapper>
       <Title>회원가입</Title>
       <SubTitle>당신을 환영합니다! 👋🏻👋🏻</SubTitle>
 
-      <IdInput type="text" placeholder="아이디" />
-      <PassWordInput type="password" placeholder="비밀번호" />
+      <IdInput
+        type="text"
+        name="email"
+        value={email}
+        onChange={onChange}
+        placeholder="아이디"
+      />
+      <PassWordInput
+        type="password"
+        name="password"
+        value={password}
+        onChange={onChange}
+        placeholder="비밀번호"
+      />
 
-      <SubmitBtn>회원가입</SubmitBtn>
+      <SubmitBtn disabled={is_disable} onClick={clickSignUp}>
+        회원가입
+      </SubmitBtn>
     </Wrapper>
   );
 }
